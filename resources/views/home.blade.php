ba7b103fe7ade2c651efc23d2a3be44061b90617
rjmchi <robert@rjmprogramming.com>
2025-09-16 19:19:44 -0600

    Initial Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<x-layout>
+    <x-slot:title>
+        Welcome
+    </x-slot:title>
+    <div class="max-w-2xl mx-auto">
+        <div class="card bg-base-100 shadow mt-8">
+
+            <div class="card-body">
+                <div>
+                    <h1 class="text-3xl font-bold">
+                        Welcome To Chirper!
+                    </h1>
+                    <p class="mt-4 text-base-content/60">This is your new app.</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</x-layout>
